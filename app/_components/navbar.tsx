9195fa891ecb035d0ae08c6aa7684f720675ef41
Limeless04 <alimvanjava@gmail.com>
2025-04-12 17:26:23 +0700

    fixing the ui for shrotener

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,13 @@
+  import React from 'react';
+
+  const Navbar = () => {
+    return (
+      <nav className="fixed top-0 left-0 right-0 h-[60px] flex items-center justify-center px-5 shadow-md">
+        <h1 className="m-0 text-2xl font-semibold text-white">
+          Simple URL Shortener
+        </h1>
+      </nav>
+    );
+  };
+export default Navbar;
+ 
